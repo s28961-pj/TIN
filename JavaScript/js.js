515407979c36e040b20b3f5693027d4a077aae3f
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2023-11-11 18:20:07 +0100

    dodaje zadanie 3 i 4

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -2,6 +2,7 @@ function zadanie1(a, b, c) {
     /* Napisz funkcję, sprawdzającą, czy dane trzy liczby tworzą
     „trójkę pitagorejską” (uwaga – liczby nie muszą być podane w
     kolejności rosnącej). */
+
     const arr = [a, b, c];
     arr.sort();
 
@@ -9,7 +10,7 @@ function zadanie1(a, b, c) {
         console.log(element);
     });
 
-    if (Math.pow(arr[0],2) + Math.pow(arr[1],2) == Math.pow(arr[2],2)) {
+    if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
         console.log("Dane liczby tworzą Trójkę Pitagorejską.");
     } else {
         console.log("Dane liczby NIE tworzą Trójki Pitagorejskiej.");
@@ -19,6 +20,7 @@ function zadanie1(a, b, c) {
 function zadanie2(a, b, c) {
     /* Napisz funkcję wypisującą wszystkie liczby z przedziału a-b,
     podzielne przez c. */
+
     const arr = [];
 
     for (let i = a; i <= b; i++) {
@@ -30,10 +32,37 @@ function zadanie2(a, b, c) {
             console.log(element);
         }
     });
-    
+
 }
 
-function zadanie3() {
+function zadanie3(a) {
     /* Napisz funkcję wypisującą w konsoli tabliczkę mnożenia o
-boku podanym jako parametr */
+    boku podanym jako parametr */
+
+    let result = '\n';
+    for (var i = 1; i <= a; i++) {
+        for (let j = 1; j <= a; j++) {
+            result += (i * j) + ' ';
+        }
+        result += '\n'
+    }
+    console.log(result);
+}
+
+function zadanie4(a) {
+    /* Napisz funkcję wypisującą w konsoli ciąg Fibonacciego o długości podanej jako parametr. */
+
+    let arr = [0, 1];
+
+    for (let i = 2; i < a; i++) {
+        arr.push(arr[i-2] + arr[i-1]);
+    }
+
+    let result = arr.toString();
+    
+    for ( let i = 0; i < a; i++) {
+        result = result.replace(",", "")
+    }
+
+    console.log(result);
 }
