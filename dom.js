500777ca2a100dfc19b6a70c7ed15f6dd78103dd
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2023-11-29 10:14:32 +0100

    zmienia strukture plikow

---
+++ dom.js
@@ -0,0 +1,11 @@
+// łączy elementy ze zmiennymi
+let firstButton = document.getElementById("firstButton");
+let secondButton = document.getElementById("secondButton");
+let thirdButton = document.getElementById("thirdButton");
+let fourthButton = document.getElementById("fourthButton");
+
+let container = document.getElementById("container");
+console.log(container.)
+
+function addDiv() {
+}
